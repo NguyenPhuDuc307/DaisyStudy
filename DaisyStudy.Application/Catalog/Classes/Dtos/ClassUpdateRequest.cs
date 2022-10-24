c3e8a04d4aa9ca9d873e0d6d557ca1626ee40d32
Nguyễn Phú Đức <95112332+NguyenPhuDuc307@users.noreply.github.com>
2022-10-24 15:58:53 +0700

    Add Image and project

---
+++ DaisyStudy.Application/Catalog/Classes/Dtos/ClassUpdateRequest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DaisyStudy.Application.Catalog.Classes.Dtos
+{
+    public interface ClassCreateRequest
+    {
+    }
+}
+
---
+++ DaisyStudy.Application/Catalog/Classes/IPublicClassService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DaisyStudy.Application.Catalog.Classes.Dtos;
+
+namespace DaisyStudy.Application.Catalog.Classes
+{
+    public interface IClassService
+    {
+        int Create(ClassCreateRequest request);
+        int Update(ClassUpdateRequest request);
+        int Delete(int ClassID);
+    }
+}
